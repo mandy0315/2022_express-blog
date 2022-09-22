6b6b23aa3c1520d54081b49358b02437af0cc597
Mandy Tsai <mandy3152618@gmail.com>
2022-09-22 11:25:27 +0800

    add: layout 共用架構

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,19 +6,19 @@ router.get("/", function (req, res, next) {
   res.redirect("/dashboard/signup");
 });
 router.get("/signup", function (req, res, next) {
-  res.render("dashboard/signup", { title: "Express" });
-});
-// 文章管理
-router.get("/archives", function (req, res, next) {
-  res.render("dashboard/archives", { title: "Express" });
+  res.render("dashboard/signup", { title: "六角部落格|後台登入" });
 });
 // 分類管理
 router.get("/categories", function (req, res, next) {
-  res.render("dashboard/categories", { title: "Express" });
+  res.render("dashboard/categories", { title: "六角部落格|分類管理" });
+});
+// 文章管理
+router.get("/archives", function (req, res, next) {
+  res.render("dashboard/archives", { title: "六角部落格|文章管理" });
 });
 // 編輯頁
 router.get("/article", function (req, res, next) {
-  res.render("dashboard/article", { title: "Express" });
+  res.render("dashboard/article", { title: "六角部落格|文章編輯" });
 });
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -4,11 +4,11 @@ var router = express.Router();
 /* main page. */
 // 首頁
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "六角部落格|首頁" });
 });
 // 文章頁
 router.get("/post", function (req, res, next) {
-  res.render("post", { title: "Express" });
+  res.render("post", { title: "六角部落格|文章內頁" });
 });
 
 module.exports = router;
